b9fa316ca9a08efdf720e0ab50f0a2369133b5aa
Daiki Maekawa <daaikii@outlook.jp>
2023-12-06 11:07:59 +0900

    jestの設定

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  moduleDirectories: [
+    'node_modules',
+    '<rootDir>/src/',
+    '<rootDir>/tests/unit/',
+  ],
+  moduleNameMapper: { '@/(.*)$': '<rootDir>/src/$1' },
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/helpers/jest-setup.ts'],
+  globalSetup: '<rootDir>/tests/unit/helpers/jest-global-setup.ts',
+  clearMocks: true,
+};
+
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ['/node_modules/(?!ip-regex)'],
+});
