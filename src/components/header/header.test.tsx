2c550f81b1161f822cbd4e81237bc54d596ddb29
Daiki Maekawa <daaikii@outlook.jp>
2023-12-12 07:38:34 +0900

    英語にする

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -4,11 +4,11 @@ import { Header } from './header';
 
 describe('header', () => {
   test('propsに応じて表示される', () => {
-    render(<Header title="新しい旅程の作成" />);
+    render(<Header title="header title" />);
 
     // 見出しが表示される
-    expect(screen.getByRole('heading', { level: 1 }).textContent).toBe(
-      '新しい旅程の作成'
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'header title'
     );
   });
 });
