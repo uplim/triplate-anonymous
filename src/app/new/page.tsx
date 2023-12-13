4f63a3b509b4e421a6d660bc0b642c5794876233
Daiki Maekawa <daaikii@outlook.jp>
2023-12-13 16:19:32 +0900

    metadataを言語によって切り替える

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,9 +3,13 @@ import { Metadata } from 'next';
 import { Header } from '@/components/header/header';
 import { getTranslation } from '@/functions/server/i18n/get-translation';
 
-export const metadata: Metadata = {
-  title: '新しい旅程の作成',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const { t } = await getTranslation('page');
+
+  return {
+    title: t('page.new.header.title'),
+  };
+}
 
 const New = async () => {
   const { t } = await getTranslation('page');
