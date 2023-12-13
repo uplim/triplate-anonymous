37e407b7873efd2e4d472f3dcff3c36d8a559396
Daiki Maekawa <daaikii@outlook.jp>
2023-12-13 15:56:10 +0900

    i18nextのproviderをstorybookにも設定

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
-import type { Preview } from '@storybook/react';
+import type { Decorator, Preview } from '@storybook/react';
 import '@/styles/globals.css';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next/initReactI18next';
+import { locales } from '../src/i18n/locales/ja';
+import { getI18nOptions } from '../src/functions/shared/i18n/get-i18n-options';
+import { I18nextProvider } from 'react-i18next';
+
+const withI18nProvider: Decorator = (Story) => {
+  i18next.use(initReactI18next).init({
+    ...getI18nOptions('ja'),
+    resources: { ja: locales },
+  });
+
+  return (
+    <I18nextProvider i18n={i18next}>
+      <Story />
+    </I18nextProvider>
+  );
+};
 
 const preview: Preview = {
   parameters: {
@@ -24,6 +42,7 @@ const preview: Preview = {
         <Story />
       </main>
     ),
+    withI18nProvider,
   ],
 };
 
