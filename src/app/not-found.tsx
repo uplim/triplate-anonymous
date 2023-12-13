e6e88ee518e4ef90f9f0623f923999fd9e021e82
Daiki Maekawa <daaikii@outlook.jp>
2023-12-13 16:30:33 +0900

    errorページ修正

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,8 @@
+import { getTranslation } from '@/functions/server/i18n/get-translation';
+
 import style from './not-found.module.css';
 
 export default async function NotFound() {
-  return <main className={style.container}>みつかりませんでした</main>;
+  const { t } = await getTranslation();
+  return <main className={style.container}>{t('error.404.description')}</main>;
 }
