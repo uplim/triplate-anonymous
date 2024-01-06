1fa95b18c31dad2abff687887c2373e6b2059563
Daiki Maekawa <daaikii@outlook.jp>
2024-01-06 21:40:20 +0900

    修正

--- src/repositories/get-firebase.ts
+++ src/repositories/get-firebase.ts
@@ -1,6 +1,12 @@
 import 'server-only';
 
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+import {
+  initializeApp,
+  cert,
+  ServiceAccount,
+  getApps,
+  getApp,
+} from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 import { cache } from 'react';
@@ -12,10 +18,13 @@ const initFirebaseAdmin = () => {
     privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   };
 
-  const app = initializeApp({
-    credential: cert(serviceAccount),
-    storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  });
+  const app =
+    getApps().length === 0
+      ? initializeApp({
+          credential: cert(serviceAccount),
+          storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+        })
+      : getApp();
 
   return app;
 };
