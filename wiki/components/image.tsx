de276489183fc8ed5ab472ea534fac6e2fcaf957
Daiki Maekawa <daaikii@outlook.jp>
2024-02-23 15:24:02 +0900

    backlink

---
+++ wiki/components/image.tsx
@@ -0,0 +1,22 @@
+type ImageProps = {
+  src: string;
+  size: number;
+};
+
+export const Image = ({ src, size }: ImageProps) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: `${size}px`,
+        height: `${size}px`,
+        padding: '4px',
+        borderRadius: '4px',
+        border: '1px solid gray',
+      }}
+    >
+      <img alt="" src={src} />
+    </div>
+  );
+};
